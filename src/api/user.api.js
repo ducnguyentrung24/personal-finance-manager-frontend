20d971712aaff6caea7fa7601a65a82c21fecac7
ducnguyentrung24 <2251061745@e.tlu.edu.vn>
2026-03-11 01:50:35 +0700

    feat(users): extend user API helpers for admin management

--- src/api/user.api.js
+++ src/api/user.api.js
@@ -8,6 +8,34 @@ const userAPI = {
 
   updateMe(data) {
     return axiosClient.patch("/auth/me", data)
+  },
+
+  getAll() {
+    return axiosClient.get("/users")
+  },
+
+  create(data) {
+    return axiosClient.post("/users", data)
+  },
+
+  update(id, data) {
+    return axiosClient.patch(`/users/${id}`, data)
+  },
+
+  delete(id) {
+    return axiosClient.delete(`/users/${id}`)
+  },
+
+  deleteMany(ids) {
+    return axiosClient.delete("/users", { data: { ids } })
+  },
+
+  lock(id) {
+    return axiosClient.patch(`/users/${id}/lock`)
+  },
+
+  unlock(id) {
+    return axiosClient.patch(`/users/${id}/unlock`)
   }
 
 }
