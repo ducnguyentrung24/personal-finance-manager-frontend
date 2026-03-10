7a5b01b9d65685b22298c662b4ba868208573995
ducnguyentrung24 <2251061745@e.tlu.edu.vn>
2026-03-11 01:28:09 +0700

    feat(frontend): add change-password API method

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -8,6 +8,10 @@ const authAPI = {
 
   register(data) {
     return axiosClient.post("/auth/register", data)
+  },
+
+  changePassword(data) {
+    return axiosClient.patch("/auth/change-password", data)
   }
 
 }
